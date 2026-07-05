d5f55972f9df1d2417ed5339bf6fda55278b47fd
Biswajit Jana <49089568+Biswajit1999@users.noreply.github.com>
2026-07-05 01:51:47 +0100

    Remove obsolete dashboard import synchroniser

--- assets/js/dashboard-import-sync.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  window.addEventListener("hi21:import-change", () => {
-    // dashboard.js redraws its provenance state on resize; reuse that single draw path.
-    window.dispatchEvent(new Event("resize"));
-  });
-})();
